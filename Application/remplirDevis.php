8d414b4a7ea0be19d1a4f77316f592f7cd380ce1
SabrinaAitMaksene <aitmaksenesabrina@gmail.com>
2018-05-15 16:37:49 +0200

    ajout devis

--- Application/remplirDevis.php
+++ Application/remplirDevis.php
@@ -2,18 +2,51 @@
 	<head>
 		<meta charset="utf-8">
     	<title> Sylvain ARD </title>
+    	<link rel="stylesheet" href="remplirdevis.css">
 	</head>
 	<body>
 		<h2>Remplir le devis </h2>
-		<form method="POST" action ="remplisDevis.php">
-			<p>Reference </p>
-			<input type ="text" name ="reference">
-			<P>Description</P>
-			<input type ="text" name ="description">
-			<P>Quantité</P>
-			<input type ="number" name ="quantite">
-			<P>Prix Unitaire</P>
-			<input type ="number" name ="pu">
-		</form>
+		<div id = "conteneur">
+		<div id ="devis">
+				<form  method ="POST" action ="remplirdevis.php">
+					<p>
+					<label for="numD">Numero devis :</label>
+					<input type ="number" id="numD">
+					</p>
+					<p>
+					<label for="dateD">Date du devis :</label>
+					<input type ="date" id="dateD">
+					</p>
+					<p>
+					<label for="url">Url Devis : </label>
+					<input type ="url" id="url">
+					</p>
+					<p>
+					<label for="NC">Numéro client :</label>
+					<input type ="number" id="NC">
+					</p>
+				</form>
+			</div>
+			<div id ="ligneD">
+				<form method="POST" action ="remplisDevis.php">
+					<p>
+						<label for="reference">Reference </label>
+						<input type ="text"  id ="reference">
+					</p>
+					<P>
+						<label for="descrip">Description</label>
+						<input type ="number"  id ="descrip">
+					</P>
+					<P>
+						<label for="quantite">Quantité</label>
+						<input type ="number"  id ="quantite">
+					</P>
+					<P>
+						<label for ="pu">Prix Unitaire</label> 
+						<input type ="text"  id ="pu"> 
+					</P>
+				</form>
+			</div>
+		</div>
 	</body>
 </html>
