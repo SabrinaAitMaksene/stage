8b2842ba1fbde29193b1ec92beb9b95b81b63e39
SabrinaAitMaksene <aitmaksenesabrina@gmail.com>
2018-05-17 11:38:36 +0200

    correction de l'insertion de données dans la base

--- Application/remplirDevis.php
+++ Application/remplirDevis.php
@@ -7,9 +7,50 @@
 	 {
 	 	die('erreur :'.$e->getMessage());
 	 }
- 	$requete = $bdd->prepare('INSERT INTO devis(numeroDevis,dateDevis,urlDevis,numeroCl,idClient) VALUES(?, ?, ?, ?, ?)');
- 	$requete->execute(array($_POST['numeroDevis'],$_POST['dateDevis'],$_POST['urlDevis'],$_POST['numeroCl'],$_POST['idClient']));
- 	echo'devis ajouté ';
+ 	
+ 	if (isset ($_POST['submit']))
+ 	{
+ 		 $requete = $bdd->prepare('INSERT INTO devis(numeroDevis,dateDevis,urlDevis,numeroCL,idClient) VALUES(:numeroDevis, :dateDevis, :urlDevis, :numeroCL, :idClient)');
+ 		 $numeroDevis=$_POST['numeroDevis'];
+ 		 $dateDevis=$_POST['dateDevis'];
+ 		 $urlDevis=$_POST['urlDevis'];
+ 		 $numeroCL=$_POST['numeroCL'];
+ 		 $idClient=$_POST['idClient'];
+ 	
+	 	 $requete->execute(array(
+	 		'numeroDevis'=>$numeroDevis,
+	 		'dateDevis'=>$dateDevis,
+	 		'urlDevis'=>$urlDevis,
+	 		'numeroCL'=>$numeroCL,
+	 		'idClient'=>$idClient
+	 	 ));
+	 		echo'devis ajouté ';
+ 	}
+
+ 	if (isset ($_POST['envoiL']))
+ 	{
+ 		 $referenceD=$_POST['referenceD'];
+ 		 $descriptionD=$_POST['descriptionD'];
+ 		 $quantiteD=$_POST['quantiteD'];
+ 		 $prixUnitaireD=$_POST['prixUnitaireD'];
+ 		 $response = $bdd->query('SELECT idDevis FROM devis where  numeroDevis == $numeroDevis');
+ 		 $donnees = $response->fetch();
+ 		 $idDevis= $donnees['idDevis'];
+ 		 $response->closeCursor();
+ 		 
+
+ 		$requete = $bdd->prepare('INSERT INTO lignedevis(referenceD,descriptionD,quantiteD,prixUnitaireD,idDevis) VALUES(:referenceD, :descriptionD, :quantiteD, :prixUnitaireD, :idDevis)');
+ 		
+ 	
+ 		$requete->execute(array(
+ 		'referenceD'=>$referenceD,
+ 		'descriptionD'=>$descriptionD,
+ 		'quantiteD'=>$quantiteD,
+ 		'prixUnitaireD'=>$prixUnitaireD,
+ 		'idDevis'=>$idDevis
+ 		));
+ 		echo'devis ajouté ';
+ 	}
 ?>
 <html>
 	<head>
@@ -21,7 +62,7 @@
 		<h2>Remplir le devis </h2>
 		<div id = "conteneur">
 			<div id ="devis">
-				<form  method ="post" action ="remplirdevis.php">
+				<form  method ="POST" action ="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
 					<p>
 						<label for="numeroDevis">Numero devis </label>
 						<input type ="number" min ="0" name="numeroDevis" id ="numeroDevis"required>
@@ -43,9 +84,32 @@
 						<input type ="number" min ="0"  name="idClient" id ="idClient"required>
 					</p>
 					<p>
-						<input type ="submit" value ="ajouter un devis" >
+						<input type ="submit" value ="ajouter un devis" name = "submit" id="submit">
 					</p>
 				</form>
+			</div>
+			<div id ="ligneD">
+				<form  method ="POST" action ="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+					<p>
+						<label for="referenceD">Reference </label>
+						<input type ="text"  name ="referenceD" id ="referenceD">
+					</p>
+					<P>
+						<label for="descriptionD">Description</label>
+						<input type ="text" name = "descriptionD" id ="descriptionD">
+					</P>
+					<P>
+						<label for="quantiteD">Quantité</label>
+						<input type ="number"  min ="0"  name ="quantiteD" id ="quantiteD">
+					</P>
+					<P>
+						<label for ="prixUnitaireD">Prix Unitaire</label> 
+						<input type ="number" min ="0" name ="prixUnitaireD"  id ="prixUnitaireD"> 
+					</P>
+					<p>
+						<input type = "submit" value="Ajouter un element " name = "envoiL" id ="envoiL">
+					</p>
+				</form>	
 			</div>
 		</div>
 	</body>
